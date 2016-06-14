1a5aff090679a76e2e37b09e3ef88ae9a59bbcd6
rneupane93 <rneupane93@hotmail.com>
2016-06-14 13:54:11 -0500

    determined the R-alloc
    Hey!! i talked to Saptarshi. He said that the value R_k_j is to be assigned to Ralloc. Please check and let me know if there is any thing or everything wrong with it. :)

--- implementation.cpp
+++ implementation.cpp
@@ -30,7 +30,19 @@ double randomize(double a, double b) {
 
     return dis(gen);
 }
-
+double assignment(double avail_j)
+{
+	double rmin_k = randomize(0, 0.5);
+	double rmax_k = randomize(0.5, 1);
+	double p_k = randomize(0, 1.0);
+	if (avail_j < rmin_k)
+		R_k_j = 0;
+	else if (p_k * rmax_k < rmin_k)
+		R_k_j = rmin_k;
+	else
+		R_k_j = p_k * rmax_k;
+	return R_k_j;
+}
 int main() {
 
   std::vector<int> vmArray{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};    // Holds the ID of every VM
@@ -39,15 +51,16 @@ int main() {
   // SC_snapshotCost_i = timeAtIdeal / timeAtSource;
   double SC_snapshotCost_i = randomize(0, 1);
   
+  double Rmax, Ralloc, U_utility;
   std::vector<double> bidArray;
   double B_bid = std::numeric_limits<double>::min();
 
-  //Compute Ralloc value for the application k.
+  //Compute Ralloc value for the application k. We only need one application
   double C_compute_k = randomize(0, 0.5); // 1 - 0.65 {i.e, 65% CPU is being used}
   double N_network_k = randomize(0, 0.5); // 1 - 0.50 {i.e, 50% bandwidth being used}
   double Ralloc_k = rootMeanSquare(C_compute_k, N_network_k);
 
-  //Compute Rmax value for the application k.
+  //Compute Rmax value for the application k. We only need one application
   double C_max_k = randomize(0.5, 1);
   double N_max_k = randomize(0.5, 1);
   double Rmax_k = rootMeanSquare(C_max_k, N_max_k);
@@ -55,12 +68,12 @@ int main() {
   for (int j = 0; j < vmArray.size(); ++j) {
     
     // BEGIN: Deployment Cost calculation
-    //Compute R value for the application k.
+    //Compute R value for the application k. We only need one application
     double C_compute_j = randomize(0, 1); // 1 - 0.65 {i.e, 65% CPU is being used}
     double N_network_j = randomize(0, 1); // 1 - 0.50 {i.e, 50% bandwidth being used}
     double Ravail_j = rootMeanSquare(C_compute_j, N_network_j);
     double Rmax_j = 1;
-    double DC_deploymentCost_j = Ravail_j / Rmax_j;
+    double DC_deploymentCost_j = R_j / Rmax_j;
     // END: Deployment Cost calculation
     
     // BEGIN: Migration Cost calculation
@@ -74,12 +87,14 @@ int main() {
 
     // BEGIN: Utility calculation
     double r_reputation_j = randomize(0, 1);
-    double U_utility_j = r_reputation_j * (Ralloc_k / Rmax_k);
+	double Ralloc_k = assignment(Ravail_j);
+	double U_utility_j = r_reputation_j * (Ralloc_k / Rmax_k);
     // END: Utility calculation
 
     // BEGIN: Bid calculation
-    bidArray.push_back(U_utility_j - MC_migrationCost_ij - DC_deploymentCost_j);
+    bidArray[j] = U_utility_j - MC_migrationCost_ij - DC_deploymentCost_j;
     // END: Bid calculation
+
   }
 
   // BEGIN: Bid selection
@@ -91,12 +106,4 @@ int main() {
 
   // Print the best VM's ID and Bid value
   std::cout << "The best VM is VM#" << vmArray[bestIndex] << " with Bid value = " << B_bid << std::endl;
-
-  std::cout << std::endl;
-  std::cout << "********" << std::endl;
-  std::cout << "Extra stats: " << std::endl;
-  std::cout << "Ralloc_k(C, N) = " << Ralloc_k;
-  std::cout << "(" << C_compute_k << ", " << N_network_k << ")" << std::endl;
-  std::cout << "Rmax_k(Cmax, Nmax) = " << Rmax_k;
-  std::cout << "(" << C_max_k << ", " << N_max_k << ")" << std::endl;
 }
